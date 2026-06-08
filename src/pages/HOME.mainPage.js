46963dafc09fd9a7db855ca8377cbf4e6786bb21
friendlyfoe241-cyber <friendlyfoe241@gmail.com>
2026-06-08 21:07:11 +0400

    initial commit

---
+++ src/pages/HOME.mainPage.js
@@ -0,0 +1,13 @@
+// Velo API Reference: https://www.wix.com/velo/reference/api-overview/introduction
+
+$w.onReady(function () {
+
+    // Print hello world:
+    // console.log("Hello world!");
+
+    // Call functions on page elements, e.g.:
+    // $w("#button1").label = "Click me!";
+
+    // Click "Run", or Preview your site, to execute your code
+
+});
